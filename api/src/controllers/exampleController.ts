030e1a679dfc682790e178c9b7ca8429c8775582
Leonardo Paglialunga <paglialungalf@gmail.com>
2022-09-08 12:03:51 -0300

    make into monorepo

--- api/src/controllers/exampleController.ts
+++ api/src/controllers/exampleController.ts
@@ -3,12 +3,17 @@ import { Controller, Get, Query, Route, Security, Request } from 'tsoa'
 
 @Route('example')
 export class ExampleController extends Controller {
-  @Get('resource')
+  @Get('protected-resource')
   @Security('jwt')
   async getProtectedResource(
     @Request() request: AuthenticatedRequest,
     @Query('param_name') paramName: string
   ) {
     return { param: request.user, paramName }
+  }
+
+  @Get('public-resource')
+  async getPublicResource() {
+    return { public: 'resource' }
   }
 }
